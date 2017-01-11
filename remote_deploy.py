a7b30d724cbba6c358bb10225e30eb47171104dc
jobs <yao050421103@gmail.com>
2017-01-11 18:20:58 +0800

    update remote_prefix.py & remote_deploy.py

--- remote_deploy.py
+++ remote_deploy.py
@@ -4,9 +4,9 @@
 import os
 import sys
 import string
+import datetime
 
 merge = lambda l: string.join(l, '\n')
-get_cmd = lambda user, cmd: 'sudo -u %s %s -oStrictHostKeyChecking=no' % (user, cmd)
 
 def manual(): 
     print """
@@ -16,38 +16,29 @@ def manual():
         python remote_deploy.py jobs host.txt /opt/huststore elf_hustdb.tar.gz
         """
 
-def get_items(uri):
-    with open(uri) as f:
-        return filter(lambda s: len(s) > 0 and not s.startswith('#'), map(
-            lambda s: s.split('\n')[0].split('\r')[0], f.readlines()))
+def remote_ssh(key, option, user, host_file, cmds):
+    cmd_file = '%s_%s.sh' % (key, datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+    with open(cmd_file, 'w') as f:
+        f.writelines(merge(cmds))
+    if os.path.exists(cmd_file):
+        os.system('python remote_ssh.py %s %s %s %s' % (option, user, host_file, cmd_file))
+        os.remove(cmd_file)
+    return True
 
-def gen_ssh_cmd(user, host, cmds):
-    return merge([
-        '%s %s@%s \\' % (get_cmd(user, 'ssh'), user, host),
-        '    \' \\',
-        merge(['    %s; \\' % cmd for cmd in cmds]),
-        '    \''
-        ])
-
-def remote_deploy(user, host, prefix, tar):
-    return merge([
-        '%s %s %s@%s:/tmp;' % (get_cmd(user, 'scp'), tar, user, host),
-        gen_ssh_cmd(user, host, [
-            'mv /tmp/%s %s' % (tar, prefix),
-            'cd %s' % prefix,
-            'tar -zxf %s -C .' % tar,
-            'rm -f %s' % tar,
-            ])
+def remote_deploy(user, host_file, prefix, tar):
+    os.system('python remote_scp.py -s %s %s /tmp %s' % (user, host_file, tar))
+    return remote_ssh('remote_deploy', '-s', user, host_file, [
+        'mv /tmp/%s %s' % (tar, prefix),
+        'cd %s' % prefix,
+        'tar -zxf %s -C .' % tar,
+        'rm -f %s' % tar
         ])
 
 def parse_shell(argv):
     size = len(argv)
     if size != 5:
         return False
-    (user, host_file, prefix, tar) = (argv[1], argv[2], argv[3], argv[4])
-    for host in get_items(host_file):
-        print host
-        os.system(remote_deploy(user, host, prefix, tar))
+    remote_deploy(argv[1], argv[2], argv[3], argv[4])
     return True
 
 if __name__ == "__main__":
--- remote_prefix.py
+++ remote_prefix.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import string
+import datetime
 
 merge = lambda l: string.join(l, '\n')
 
@@ -15,21 +16,17 @@ def manual():
         python remote_prefix.py jobs host.txt /opt/huststore jobs
         """
 
-def get_items(uri):
-    with open(uri) as f:
-        return filter(lambda s: len(s) > 0 and not s.startswith('#'), map(
-            lambda s: s.split('\n')[0].split('\r')[0], f.readlines()))
+def remote_ssh(key, option, user, host_file, cmds):
+    cmd_file = '%s_%s.sh' % (key, datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+    with open(cmd_file, 'w') as f:
+        f.writelines(merge(cmds))
+    if os.path.exists(cmd_file):
+        os.system('python remote_ssh.py %s %s %s %s' % (option, user, host_file, cmd_file))
+        os.remove(cmd_file)
+    return True
 
-def gen_ssh_cmd(user, host, cmds):
-    return merge([
-        'ssh %s@%s \\' % (user, host),
-        '    \' \\',
-        merge(['    %s; \\' % cmd for cmd in cmds]),
-        '    \''
-        ])
-
-def remote_prefix(user, host, prefix, owner):
-    return gen_ssh_cmd(user, host, [
+def remote_prefix(user, host_file, prefix, owner):
+    return remote_ssh('remote_prefix', '', user, host_file, [
         'test -d %s || sudo -S mkdir -p %s' % (prefix, prefix),
         'sudo chown -R %s:%s %s' % (owner, owner, prefix)
         ])
@@ -38,11 +35,7 @@ def parse_shell(argv):
     size = len(argv)
     if size != 5:
         return False
-    (user, host_file, prefix, owner) = (argv[1], argv[2], argv[3], argv[4])
-    for host in get_items(host_file):
-        print host
-        os.system(remote_prefix(user, host, prefix, owner))
-    return True
+    return remote_prefix(argv[1], argv[2], argv[3], argv[4])
 
 if __name__ == "__main__":
     if not parse_shell(sys.argv):

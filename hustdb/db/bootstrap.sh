e18c740943255fb9cb77e682ec0c116ecd538229
Tian Congxin <tiancongxin@gmail.com>
2017-01-12 16:09:55 +0800

    automake use copy (default symlink)
    * tarball elf*gz path change to topsrc
    * check before compiling 3rd

--- hustdb/db/bootstrap.sh
+++ hustdb/db/bootstrap.sh
@@ -2,13 +2,10 @@
 set -e
 set -x
 
-
 touch NEWS README ChangeLog AUTHORS COPYING INSTALL
-cp -v  /usr/share/automake-1.*/{depcomp,compile}  ./
 
 aclocal
 autoconf
 autoheader
-libtoolize --automake --copy --debug --force
-automake --add-missing
-
+libtoolize --automake --copy --force
+automake --add-missing --copy

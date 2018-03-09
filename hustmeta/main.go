c5b57364c7cb1a816de7d0c35400b7d85460f403
jobs <yao050421103@163.com>
2018-03-09 16:52:20 +0800

    update main.go

--- hustmeta/main.go
+++ hustmeta/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/cihub/seelog"
 )
 
 func main() {
-	fmt.Println("hustmeta")
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	root := filepath.Dir(path)
+
+	var conf string
+	flag.StringVar(&conf, "conf", "", "")
+	flag.Parse()
+
+	if "" == conf {
+		conf = filepath.Join(root, "conf")
+	} else {
+		conf, _ = filepath.Abs(conf)
+	}
+
+	logger, err := seelog.LoggerFromConfigAsFile(filepath.Join(conf, "seelog.xml"))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	seelog.ReplaceLogger(logger)
+	defer seelog.Flush()
+
+	seelog.Debug("hustmeta start...")
 }

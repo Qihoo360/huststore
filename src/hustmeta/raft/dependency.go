2e1dfff26035eef6db36ff4f131aee90774071a7
jobs <yao050421103@163.com>
2018-04-11 14:31:05 +0800

    init fsm & snapshot

---
+++ src/hustmeta/raft/dependency.go
@@ -0,0 +1,10 @@
+package raft
+
+// "def.go": []
+// "config.go": []
+// "configuration.go" : ["def.go"]
+// "commitment.go": ["def.go"]
+// "log.go": []
+// "commands.go": ["log.go"]
+// "future.go": ["commands.go"]
+// "transport.go": ["future.go"]
